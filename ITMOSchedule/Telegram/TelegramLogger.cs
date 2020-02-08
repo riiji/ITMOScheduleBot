e3f17f6673cba5cb9205cff312eda0dfb6e54b3b
Sergey <I_SER_I@hotmail.com>
2020-02-08 19:47:21 +0300

    Add TelegramLogger
    empty Login/Logout function

---
+++ ITMOSchedule/Telegram/TelegramLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ITMOSchedule.Bot.Interfaces;
+using Telegram.Bot;
+using MihaZupan;
+
+namespace ITMOSchedule.Bot
+{
+    public class TelegramLogger : ILogger
+    {
+        private readonly TelegramBotClient _TelegramApi;
+
+        public TelegramLogger(TelegramBotClient _TelegramApi)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
