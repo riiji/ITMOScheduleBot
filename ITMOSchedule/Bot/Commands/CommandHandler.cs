2f1385fa619f6ace997599b3f6e0452cdca95eab
Riijii <riijii333@gmail.com>
2020-02-08 21:12:46 +0300

    added command system

---
+++ ITMOSchedule/Bot/Commands/CommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using ITMOSchedule.Bot.Exceptions;
+using ITMOSchedule.Commands;
+
+namespace ITMOSchedule.Bot.Commands
+{
+    public class CommandHandler
+    {
+        public void ExecuteCommand(string commandName, string[] args)
+        {
+            var command = CommandsList.GetCommand(commandName);
+
+            command.Execute(args);
+        }
+    }
+}
