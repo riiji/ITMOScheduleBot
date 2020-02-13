5e81fcaca40756ff8d3586e21e8207d8782543cb
Riijii <riijii333@gmail.com>
2020-02-13 19:19:34 +0300

    added settings

---
+++ ITMOSchedule/Telegram/TelegramSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MihaZupan;
+
+namespace ITMOSchedule.Telegram
+{
+    public static class TelegramSettings
+    {
+        public static string Key { get; set; }
+        public static HttpToSocks5Proxy Proxy { get; set; }
+    }
+}
---
+++ ITMOSchedule/VK/VkSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ITMOSchedule.Bot
+{
+    public static class VkSettings
+    {
+        public static string Key { get; set; }
+
+        public static int AppId { get; set; }
+
+        public static string AppSecret { get; set; }
+
+        public static int GroupId { get; set; }
+    }
+}
