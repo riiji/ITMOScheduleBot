1f769af946c3bac2a9d65598605755ea8955b13e
Riijii <riijii333@gmail.com>
2020-03-28 12:05:51 +0300

    test: ping command test added

---
+++ ItmoSchedule.Tests/Commands_Should/PingCommand_Should.cs
@@ -0,0 +1,20 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using ItmoSchedule.BotCommands;
+using ItmoSchedule.Common;
+using NUnit.Framework;
+
+namespace ItmoSchedule.Tests.Commands_Should
+{
+    public class PingCommand_Should
+    {
+        [Test]
+        public void PingCommand_ShouldReturnPong()
+        {
+            PingCommand pingCommand = new PingCommand();
+            CommandArgumentContainer fakeContainer = A.Fake<CommandArgumentContainer>();
+            pingCommand.Execute(fakeContainer).IsSuccess.Should().Be(true);
+        }
+
+    }
+}
